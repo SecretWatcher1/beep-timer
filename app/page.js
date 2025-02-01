a27b46f51db459040cb83413352ddbb1f17c0f04
Khalid <khalood.alhawaiti@gmail.com>
2025-02-01 05:05:55 +0300

    Add simple test app

--- app/page.js
+++ app/page.js
@@ -1,8 +1,33 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 export default function Home() {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCounter((prev) => prev + 1); // Increment counter every second
+    }, 1000);
+
+    return () => clearInterval(interval); // Cleanup on unmount
+  }, []);
+
   return (
-    <div>
-      <h1>Hellooo, World!</h1>
-      <p>This is your updated Next.js app!</p>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "100vh",
+        fontFamily: "Arial, sans-serif",
+        textAlign: "center",
+      }}
+    >
+      <h1>Hello, World!</h1>
+      <p>This is your very simple test app.</p>
+      <p>Counter: {counter}</p>
     </div>
   );
 }
