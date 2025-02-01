05d80018ce0c068db8a2444993a23cf41d966832
Khalid <khalood.alhawaiti@gmail.com>
2025-02-01 04:53:43 +0300

    Re-trigger Vercel deployment

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1>Hello, World!</h1>
+      <h1>Hellooo, World!</h1>
       <p>This is your updated Next.js app!</p>
     </div>
   );
